174ea79b809947c78980ebd3e44f092adbb96e92
Emincan YILMAZ <emincann.yilmaz@gmail.com>
2022-08-01 09:35:55 +0300

    v4 Working too fast to find a number whether prime or not.

--- isPrimFunc.py
+++ isPrimFunc.py
@@ -1,9 +1,9 @@
 def isPrime(num):
-    from math import sqrt as sqrt
+    from math import sqrt as sq
     if num < 2 or type(num) == float:
         return False
     else:
-        for i in range(2, int(sqrt(num)+1)):
+        for i in range(2, int(sq(num)+1)):
             if num % i == 0:
                 return False
         return True
